7223652e22fec6b9147042c81e3d382ddf024a82
t1msn <tim_stehle@posteo.de>
2020-04-04 23:55:39 +0200

    add navbar

---
+++ frontend/components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="nav">
+    <nuxt-link to="/" class="brand">
+      RPA
+    </nuxt-link>
+    <nav>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+.nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 80px;
+}
+.nav .nav-item {
+  box-sizing: border-box;
+  margin: 0 5px;
+  color: rgba(0, 0, 0, 0.5);
+  text-decoration: none;
+}
+
+.nav a {
+  display: inline-block;
+}
+</style>
